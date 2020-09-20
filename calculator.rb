cfca5a6595b428ef7f880aa69c93248ae6d680d4
Matt Bugay <mjbugay@yahoo.com>
2020-09-20 03:55:05 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 first_number = 1
 second_number = 2
 var sum = first_number + second_number
+var difference = second_number - first_number
+var product = first_number * second_number
+var quotient = second_number / first_number
